339f2b5b0c581f3fa1817774452c6a3952c2eea2
Hugo Gabriel Carvalho Ferreira <a2020128305@isec.pt>
2022-01-16 22:50:49 +0000

    Nothing relevant

--- Hibrido/utils.h
+++ Hibrido/utils.h
@@ -5,27 +5,16 @@
 #ifndef EVOLUTIVO_UTILS_H
 #define EVOLUTIVO_UTILS_H
 void imprime_matriz(int *grafo, int vert);
-
 void logtofile(chrom b, struct  info x,float mbf);
-
 int* init_dados(char *nome, int *n,int *lig);
-
 void init_data(char *filename, int *mat, struct  info *x );
-
 pchrom init_pop(struct info);
-
 void print_pop(pchrom, struct info);
-
 chrom get_best(pchrom, struct info, chrom);
-
 void write_best(chrom, struct info);
-
 void init_rand();
-
 int random_l_h(int, int);
-
 float rand_01();
-
 int flip();
 
 #endif //EVOLUTIVO_UTILS_H

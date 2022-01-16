1316ec4d1d49252df1681b50d4a9707c53eef358
Hugo Ferreira <84676077+hooper1337@users.noreply.github.com>
2022-01-16 23:25:47 +0000

    Delete Evolutivo/cmake-build-debug directory

--- Evolutivo/cmake-build-debug/CMakeFiles/Evolutivo.dir/funcao.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Evolutivo.dir/funcao.c.o: \
- /cygdrive/c/Optimization_Problems/Evolutivo/funcao.c \
- /cygdrive/c/Optimization_Problems/Evolutivo/algoritmo.h \
- /cygdrive/c/Optimization_Problems/Evolutivo/funcao.h \
- /cygdrive/c/Optimization_Problems/Evolutivo/utils.h
